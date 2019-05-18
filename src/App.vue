d26658fd1929418d203f91b45837dd54496b667e
Budi Syahiddin <budisyahiddin@protonmail.ch>
2019-05-18 17:22:14 +0800

    Added mobile navbar

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,26 @@
     </div>
 </template>
 
+<script>
+    import Vue from 'vue'
+    import {Component} from 'vue-property-decorator'
+    import Navbar from './components/Navbar'
+
+    @Component({
+        components: {Navbar},
+    })
+    export default class App extends Vue {}
+</script>
+
 <style lang="scss">
-    @import url("~bulma");
+    @import url("~bulma/css/bulma.css");
 
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         color: #2c3e50;
+        padding: 2em 1.5em;
     }
 
     #nav {
@@ -28,13 +40,3 @@
         }
     }
 </style>
-<script>
-    import Vue from 'vue'
-    import {Component} from 'vue-property-decorator'
-    import Navbar from './components/Navbar'
-
-    @Component({
-        components: {Navbar},
-    })
-    export default class App extends Vue {}
-</script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
         <div class="is-hidden-desktop">
-
-        </div>
-        <div class="is-hidden-mobile">
-            <router-link v-for="l in links" :to="l.to">{{l.str}}</router-link>
+            <i class="fas fa-hamburger is-size-3" @click="toggleMenuOpen"></i>
+            <div :class="menuShowing" class="mobile-menu">
+                <a v-for="l in links" class="is-size-3" @click="navigate(l.to)">
+                    <i class="fas" :class="l.iconClass"></i>
+                    {{l.str}}
+                </a>
+                <a class="is-size-3">
+                    <i class="fas fa-times" @click="toggleMenuOpen"></i>
+                </a>
+            </div>
         </div>
     </div>
 </template>
@@ -27,17 +33,51 @@
     @Component({})
     export default class Navbar extends Vue {
         links: Link[] = []
+        menuOpen = false
 
         created() {
             this.links.push(
-                new Link("Home", "/", ""),
-                new Link("Login", "/login", ""),
-                new Link("Dashboard", "/dashboard", "")
+                new Link("Home", "/", "fa-home"),
+                new Link("Login", "/login", "fa-sign-in-alt"),
+                new Link("Dashboard", "/dashboard", "fa-tachometer-alt")
             )
+        }
+
+        toggleMenuOpen() {
+            this.menuOpen = !this.menuOpen
+        }
+
+        navigate(navLink: string) {
+            this.toggleMenuOpen()
+            this.$router.push(navLink)
+        }
+
+        get menuShowing() {
+            return this.menuOpen ? '' : 'is-hidden'
         }
     }
 </script>
 
 <style lang="scss" scoped>
+    @import url("~@fortawesome/fontawesome-free/css/all.css");
 
+    .mobile-menu {
+        text-align: center;
+        position: fixed;
+        min-height: 100vh;
+        min-width: 100vw;
+        top: 0;
+        left: 0;
+        background-color: #FFC0CB;
+        padding-top: 50%;
+
+        a {
+            display: block;
+        }
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+    }
 </style>
