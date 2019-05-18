e1ce807411dc8af7d42ed0c6637cd49c3ebdac5a
Budi Syahiddin <budisyahiddin@protonmail.ch>
2019-05-18 18:50:57 +0800

    Pushed images

---
+++ src/components/NextMealCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="tinted-img"></div>
+        <div class="next-meal-card box">
+            <p class="is-size-4">Next meal</p>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator'
+
+    @Component({})
+    export default class NextMealCard extends Vue {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .next-meal-card {
+        background-image: url("../assets/salad.jpg");
+        background-size: cover;
+        min-height: 40vh;
+    }
+
+    .tinted-img {
+        background-color: #00000032;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="home">
-        <p>Home</p>
+        <br>
+        <NextMealCard></NextMealCard>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator'
-    import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
+    import NextMealCard from "@/components/NextMealCard.vue" // @ is an alias to /src
 
     @Component({
         components: {
+            NextMealCard,
             HelloWorld,
         },
     })
